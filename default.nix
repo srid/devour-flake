b43b6533dbf16f6f5388c0efd694f419bed4363b
Sridhar Ratnakumar <srid@srid.ca>
2023-04-24 15:36:45 -0400

    seems to require #default

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ pkgs.writeShellApplication {
   name = "devour-flake-cat";
   runtimeInputs = [ pkgs.nix ];
   text = ''
-    nix build ${devour-flake} \
+    nix build ${devour-flake}#default \
       -L --no-link --print-out-paths \
       --override-input flake "$1" \
       | xargs cat 
