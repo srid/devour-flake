0d9b1661184d40c09a21ce4db0e526e923a2e1b5
Sridhar Ratnakumar <srid@srid.ca>
2023-07-13 11:46:46 -0400

    Refactor further, for perSystem distinction

--- flake.nix
+++ flake.nix
@@ -11,21 +11,26 @@
       perSystem = { self', pkgs, lib, system, ... }: {
         packages.default =
           let
-            lookupFlake = k:
-              lib.attrByPath [ k system ] { };
             # Given a flake output key, how to get the buildable derivation for
             # any of its attr values?
-            drvFor = {
-              packages = x: x;
-              checks = x: x;
-              devShells = x: x;
-              apps = app: app.program;
+            flakeSchema = {
+              perSystem = {
+                lookupFlake = k: lib.attrByPath [ k system ] { };
+                getDrv = {
+                  packages = x: x;
+                  checks = x: x;
+                  devShells = x: x;
+                  apps = app: app.program;
+                };
+              };
             };
             paths =
-              lib.flip lib.mapAttrsToList drvFor (kind: getDrv:
-                builtins.map
-                  getDrv
-                  (lib.attrValues (lookupFlake kind inputs.flake)));
+              lib.flip lib.mapAttrsToList flakeSchema (lvl: lvlSchema:
+                lib.flip lib.mapAttrsToList lvlSchema.getDrv (kind: getDrv:
+                  builtins.map
+                    getDrv
+                    (lib.attrValues (lvlSchema.lookupFlake kind inputs.flake)))
+              ) ;
           in
           pkgs.runCommand "devour-output" { inherit paths; } ''
             echo -n $paths > $out
