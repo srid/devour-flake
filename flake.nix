b38be492c825229669a0755ceea4618f9f872c08
Sridhar Ratnakumar <srid@srid.ca>
2023-07-13 11:40:49 -0400

    Refactor: generalize

--- flake.nix
+++ flake.nix
@@ -13,13 +13,19 @@
           let
             lookupFlake = k:
               lib.attrByPath [ k system ] { };
-            paths = lib.concatMap lib.attrValues [
-              (lookupFlake "packages" inputs.flake)
-              (lookupFlake "checks" inputs.flake)
-              (lookupFlake "devShells" inputs.flake)
-              (lib.mapAttrs (_: app: app.program)
-                (lookupFlake "apps" inputs.flake))
-            ];
+            # Given a flake output key, how to get the buildable derivation for
+            # any of its attr values?
+            drvFor = {
+              packages = x: x;
+              checks = x: x;
+              devShells = x: x;
+              apps = app: app.program;
+            };
+            paths =
+              lib.flip lib.mapAttrsToList drvFor (kind: getDrv:
+                builtins.map
+                  getDrv
+                  (lib.attrValues (lookupFlake kind inputs.flake)));
           in
           pkgs.runCommand "devour-output" { inherit paths; } ''
             echo -n $paths > $out
