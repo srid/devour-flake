d3968c27df1afa35a2e7437960a3662a9877399b
Ivan Petkov <ivanppetkov@gmail.com>
2023-09-03 13:54:02 -0700

    Add default package which includes both scripts

--- flake.nix
+++ flake.nix
@@ -8,7 +8,15 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
       perSystem = { self', pkgs, lib, system, ... }: {
-        packages = {
+        packages = rec {
+          default = pkgs.symlinkJoin {
+            name = "devour-flake";
+            paths = [
+              devour-flake
+              devour-flake-uncached
+            ];
+          };
+
           # Build all derivations in a flake (or download them from the cache)
           devour-flake = pkgs.callPackage ./pkgs/devour-flake.nix { };
           # Only build all uncached derivations in flake
