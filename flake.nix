bb0e0443291899a3f3fd85f87d2849f917692b1e
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-02-24 08:56:40 -0500

    Allow building multiple systems (#24)

--- flake.nix
+++ flake.nix
@@ -1,22 +1,33 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
+    allowed-systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    # The systems to build for. If empty, build for current system.
+    systems.url = "github:srid/empty";
     flake = { };
   };
-  outputs = inputs@{ flake-parts, systems, ... }:
+  outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import systems;
+      systems = import inputs.allowed-systems;
       perSystem = { self', pkgs, lib, system, ... }: {
         packages.default =
           let
-            configForCurrentSystem = cfg: cfg.config.nixpkgs.hostPlatform.system == system;
+            build-systems =
+              let systems = import inputs.systems;
+              in if systems == [ ] then [ system ] else systems;
+            shouldBuildOn = s: lib.elem s build-systems;
+            configForCurrentSystem = cfg:
+              shouldBuildOn cfg.config.nixpkgs.hostPlatform.system;
             # Given a flake output key, how to get the buildable derivation for
             # any of its attr values?
             flakeSchema = {
               perSystem = {
-                lookupFlake = k: lib.attrByPath [ k system ] { };
+                # -> [ path ]
+                lookupFlake = k: flake:
+                  lib.flip builtins.map build-systems (sys:
+                    lib.attrByPath [ k sys ] { } flake
+                  );
                 getDrv = {
                   packages = _: x: [ x ];
                   checks = _: x: [ x ];
@@ -30,21 +41,22 @@
                 };
               };
               flake = {
-                lookupFlake = k: lib.attrByPath [ k ] { };
+                lookupFlake = k: flake: [ (lib.attrByPath [ k ] { } flake) ];
                 getDrv = {
                   nixosConfigurations = _: cfg:
                     lib.optional (configForCurrentSystem cfg) cfg.config.system.build.toplevel;
-                  darwinConfigurations = _: cfg: 
+                  darwinConfigurations = _: cfg:
                     lib.optional (configForCurrentSystem cfg) cfg.config.system.build.toplevel;
                 };
               };
             };
             paths =
-              lib.flip lib.mapAttrsToList flakeSchema (lvl: lvlSchema:
+              lib.flip lib.mapAttrsToList flakeSchema (_: lvlSchema:
                 lib.flip lib.mapAttrsToList lvlSchema.getDrv (kind: getDrv:
-                  lib.mapAttrsToList
-                    getDrv
-                    (lvlSchema.lookupFlake kind inputs.flake))
+                  builtins.concatMap
+                    (attr: lib.mapAttrsToList getDrv attr)
+                    (lvlSchema.lookupFlake kind inputs.flake)
+                )
               );
           in
           pkgs.writeText "devour-output" (lib.strings.concatLines (lib.lists.flatten paths));
